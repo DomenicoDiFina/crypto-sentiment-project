1c3b92174942dc2557fad6fda7aed1829a70b42c
Domenico Di Fina <difinadomenico98@gmail.com>
2021-05-29 16:44:44 +0200

    test twint api

--- twint_test.py
+++ twint_test.py
@@ -2,7 +2,8 @@ import twint
 import pandas as pd
 
 
-# Configure
+############# Ricerca Tweet ###################
+
 c = twint.Config()
 c.Search = "etherium"
 c.Lang = "en"
@@ -14,6 +15,12 @@ c.Hide_output = True
 # Run
 twint.run.Search(c)
 
+########### Numero followers ################
+c_user = twint.Config()
+c_user.Username = "mimmodifina"
+c.Store_csv = True
+c.Output = "followers.csv"
+twint.run.Followers(c_user) # questo non funziona più a quanto pare
 
 
 df = pd.read_csv('test.csv')
