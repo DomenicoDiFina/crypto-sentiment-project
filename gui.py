b2968f067c2cae8e9dabbae0b4430725777d6452
Domenico Di Fina <difinadomenico98@gmail.com>
2021-05-29 17:53:09 +0200

    update

--- gui.py
+++ gui.py
@@ -3,9 +3,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 #import mplfinance as mpf
 import datetime as dt
+from datetime import date, timedelta
 
-start = dt.datetime(2020,1,1)
-end = dt.datetime.now()
+start = date.today() - timedelta(days=1)
+end = date.today()
 
 
 st.title("Cryptocurrency Visualizer")
@@ -13,8 +14,10 @@ st.title("Cryptocurrency Visualizer")
 st.sidebar.write(""" ## Visualizzatore Criptovalute """)
 
 crypto_name = st.sidebar.selectbox("Seleziona la crypto",('Bitcoin BTC', 'Ethereum ETH', 'Ripple XRP', 'Binance Coin BNB', 'Tether USDT', 'Cardano ADA', 'Dogecoin DOGE', 'Polkadot DOT', 'Internet Computer ICP', 'XRP', 'Uniswap UNI', 'Polygon MATIC', 'Stellar XLM', 'Litecoin LTC', 'VeChain VET', 'Solana SOL', 'SHIBA INU SHIB'))
+date_start = st.sidebar.date_input('Data Iniziale', start)
+date_end = st.sidebar.date_input('Data Finale', end)
 
 if st.sidebar.button("Visualizza"):
-    st.write(f"Grafico riguardante {crypto_name}.")
+    st.write(f"Grafico riguardante {crypto_name} dal {date_start} al {date_end}.")
 
     # st.pyplot(fig) # per inserire il la figure matplotlib nella finestra
--- twint_test.py
+++ twint_test.py
@@ -5,9 +5,12 @@ import pandas as pd
 ############# Ricerca Tweet ###################
 
 c = twint.Config()
-c.Search = "etherium"
-c.Lang = "en"
-c.Limit = 100
+c.Search = "crypto"
+c.Lang = "en" # non sembra funzionare
+c.Limit = 1000
+c.Since = '2021-05-28'
+c.Until = '2021-05-29'
+c.Verified = True # True se vogliamo i verificati, False altrimenti
 c.Output = "./test.csv"
 c.Store_csv = True
 c.Hide_output = True
@@ -15,14 +18,26 @@ c.Hide_output = True
 # Run
 twint.run.Search(c)
 
-########### Numero followers ################
-c_user = twint.Config()
-c_user.Username = "mimmodifina"
-c.Store_csv = True
-c.Output = "followers.csv"
-twint.run.Followers(c_user) # questo non funziona più a quanto pare
+# # ########### Numero followers ################
+# # c_user = twint.Config()
+# # c_user.Username = "mimmodifina"
+# # c.Store_csv = True
+# # c.Output = "followers.csv"
+# # twint.run.Followers(c_user) # questo non funziona più a quanto pare
 
 
-df = pd.read_csv('test.csv')
+#df = pd.read_csv('test.csv')
 
-df = df.loc[:,['date','time', 'user_id', 'username', 'tweet', 'language', 'hashtags']]
+#df = df.loc[:,['date','time', 'user_id', 'username', 'tweet', 'language', 'hashtags', 'retweets_count']]
+
+
+# from bs4 import BeautifulSoup
+# import requests
+# handle = "mimmodifina" 
+# temp = requests.get('https://twitter.com/mimmodifina')
+# bs = BeautifulSoup(temp.text,'lxml')
+# follow_box = bs.find('li',{'class':'ProfileNav-item ProfileNav-item--followers'})
+# followers = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
+# print("Number of followers: {} ".format(followers.get('data-count')))
+
+#print(len(df[df['retweets_count'] > 0]))
