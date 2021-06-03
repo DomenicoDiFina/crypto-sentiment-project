c4835fc66ab153f5e5701880c384eb19df2dc046
Domenico Di Fina <difinadomenico98@gmail.com>
2021-06-03 19:26:43 +0200

    update

--- get_tweets.py
+++ get_tweets.py
@@ -207,7 +207,7 @@ def create_sentiment_plot(sentiment_list):
             if sentiment[0] == day:
                 y[-1] += sentiment[1]
 
-    x = [x.strftime("%d-%m") for x in list(daterange(start_date, end_date))]
+    x = [x.strftime("%d-%m") for x in list(daterange(start_date, end_date + timedelta(days=1)))]
    
     #plotting   
     plt.style.use('dark_background')
